1dca0c09703429d49f9998d051e687177df2da3b
summer-breeze <chengjie.shen@hotmail.com>
2016-01-13 00:30:23 +0800

    增加重置功能模块的方法

---
+++ Yun.User/Request/ResetFunctionsRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+
+namespace Yun.User.Request
+{
+    /// <summary>
+    /// 重置后台功能
+    /// chenggou.permission.function.rest
+    /// </summary>
+    public class ResetFunctionsRequest :ITopRequest<BoolResultResponse>
+    {
+        public string GetApiName()
+        {
+            return "chenggou.permission.function.rest";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary();
+            return parameters;
+        }
+
+        public void Validate()
+        {
+        }
+    }
+}
