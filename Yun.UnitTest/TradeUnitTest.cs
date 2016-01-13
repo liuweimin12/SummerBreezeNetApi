c7a5a05241ebd368d635ac066dd87218b9127c6e
summer-breeze <chengjie.shen@hotmail.com>
2016-01-13 13:32:57 +0800

    增加单元测试

---
+++ Yun.UnitTest/TradeUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yun.Trade.Request;
+
+namespace Yun.UnitTest
+{
+    /// <summary>
+    /// TradeUnitTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class TradeUnitTest
+    {
+        [TestMethod]
+        public void ExecuteTradeStatisticsRequest()
+        {
+            var req = YunClient.Instance.Execute(new ExecuteTradeStatisticsRequest { Day = 30 }, YunClient.GetAdminToken());
+            Assert.IsTrue(req.Result);
+        }
+    }
+}
