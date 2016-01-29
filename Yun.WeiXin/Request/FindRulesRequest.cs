071cd1410e0153195a690aa741df9309f2b3e369
夏日微风 <chengjie.shen@hotmail.com>
2016-01-29 12:11:58 +0800

    增加微信获取所有回复接口

---
+++ Yun.WeiXin/Request/FindRulesRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Yun.Interface;
+using Yun.Util;
+using Yun.WeiXin.Response;
+
+namespace Yun.WeiXin.Request
+{
+    /// <summary>
+    /// 获取所有回复规则
+    /// chenggou.weixin.rules.find
+    /// </summary>
+    public class FindRulesRequest : ITopRequest<FindRulesResponse>
+    {
+        /// <summary>
+        /// 微信公众号ID
+        /// </summary>
+        public int AccountId { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int? Status { get; set; }
+
+
+
+        public string GetApiName()
+        {
+            return "chenggou.weixin.rules.find";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"accountid", AccountId},
+                {"status", Status}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("accountid", AccountId);
+            RequestValidator.ValidateMinValue("accountid", AccountId, 1);
+        }
+    }
+}
---
+++ Yun.WeiXin/Response/FindRulesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using Yun.Response;
+
+namespace Yun.WeiXin.Response
+{
+    [XmlRoot("find_rules_response")]
+    public class FindRulesResponse : YunResponse
+    {
+        [XmlArray("rules")]
+        [XmlArrayItem("rule")]
+        public List<RuleDomain> Rules { get; set; }
+    }
+}
