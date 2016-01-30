52d42e08afb83e65ce6d7ede6e601719fb711df4
夏日微风 <chengjie.shen@hotmail.com>
2016-01-30 16:54:22 +0800

    增加移除代金券绑定的方法

---
+++ Yun.Marketing/Request/RemoveBindingCashCouponRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Marketing.Request
+{
+    /// <summary>
+    /// 解除代金券的绑定
+    /// chenggou.cashcoupon.removebinding
+    /// </summary>
+    public class RemoveBindingCashCouponRequest:ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 代金券ID
+        /// </summary>
+        public int Id { get; set; }
+
+        public string GetApiName()
+        {
+            return "chenggou.cashcoupon.removebinding";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"id",Id}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("id", Id);
+            RequestValidator.ValidateMinValue("id", Id, 1);
+        }
+    }
+}
