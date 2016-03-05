6c39a643919879237269150a59ed0884194233d9
summer-breeze <chengjie.shen@hotmail.com>
2016-03-05 11:52:47 +0800

    备注增加内容

--- Yun.Trade/MemoModel.cs
+++ Yun.Trade/MemoModel.cs
@@ -4,10 +4,22 @@ namespace Yun.Trade
 {
     public class MemoModel
     {
+        /// <summary>
+        /// 主订单标记
+        /// </summary>
         [XmlElement("flag")]
         public int Flag { get; set; }
 
+        /// <summary>
+        /// 卖家备注
+        /// </summary>
         [XmlElement("remark")]
         public string Remark { get; set; }
+
+        /// <summary>
+        /// 自定义订单类型，卖家直接控制，和订单标记联合使用
+        /// </summary>
+        [XmlElement("slave_flag")]
+        public int SlaveFlag { get; set; }
     }
 }
