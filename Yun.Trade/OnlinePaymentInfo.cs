6aa949b89723f761866cf14d3866e1bd3f7802ce
summer-breeze <chengjie.shen@hotmail.com>
2016-03-15 23:36:02 +0800

    更新订单中在线付款返回类型

--- Yun.Trade/OnlinePaymentInfo.cs
+++ Yun.Trade/OnlinePaymentInfo.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace Yun.Trade
 {
@@ -24,20 +20,20 @@ namespace Yun.Trade
         /// 第三方支付代码/名字
         /// </summary>
         [XmlElement("pay_code")]
-        public KeyValuePair<string, string> PayCode { get; set; }
+        public string PayCode { get; set; }
 
         /// <summary>
         /// 银行代码
         /// </summary>
         [XmlElement("pay_bank")]
-        public KeyValuePair<string, string> PayBank { get; set; }
+        public string PayBank { get; set; }
 
 
         /// <summary>
         /// 支付渠道，用来区分信用卡和储蓄卡支付
         /// </summary>
         [XmlElement("pay_channel")]
-        public KeyValuePair<string, string> PayChannel { get; set; }
+        public string PayChannel { get; set; }
 
         /// <summary>
         /// 支付商的交易号
