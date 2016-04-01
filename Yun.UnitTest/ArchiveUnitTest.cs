fcf97d5a612418c47beddf64df693d4255b3c241
zhongluantian <tianzhongluan@163.com>
2016-04-01 16:26:24 +0800

    更新Archive

--- Yun.UnitTest/ArchiveUnitTest.cs
+++ Yun.UnitTest/ArchiveUnitTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Yun.User.Archive;
+using Yun.Archive.Request;
 
 
 namespace Yun.UnitTest
@@ -13,16 +13,67 @@ namespace Yun.UnitTest
         {
             YunClient.Format = "json";
             var req =
-                YunClient.Instance.Execute(new g
+                YunClient.Instance.Execute(new GetArchivesRequest
                 {
-                    Ip = "192.168.1.1",
-                    Phone = "12345678996",
-                    Code = "888999",
-                    ShopId = 123,
-                    CompanyId = 123
+                    PageNum = 1,
+                    PageSize = 10,
+                    AuthorId = 123456,
+                    Sort = "123",
+                    Title = "titlt",
+                    StartPostTime=1,
+                    EndPostTime=2,
+                    TaxonomyId=123,
+                    Fields="fields",
+                    ShopId=123,
+                    Status="status",
+                    ///HasPunish=,
+                    Tags="tags",
+                    CategoryName="hello",
+                    CompanyId=123
+                    
                 });
+            Assert.IsTrue(req != null);
+        }
+        [TestMethod]
+        public void GetArchiveRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetArchiveRequest
+                {
+                    Id = 1,
+                    Fields = "fields"
+                    
+                });
+            Assert.IsTrue(req != null);
+        }
+        [TestMethod]
+        public void GetCategoriesRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetCategoriesRequest
+                {
+                    Display = 1,
+                    ShopId= 123,
+                    ParentId=123
 
-            Assert.IsTrue(req.UserId > 0);
+                });
+            Assert.IsTrue(req != null);
+        }
+        [TestMethod]
+        public void GetCategoryRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetCategoryRequest
+                {
+                    Id = 123,
+                    
+                });
+            Assert.IsTrue(req != null);
+        }
+       
         }
     }
-}
+
