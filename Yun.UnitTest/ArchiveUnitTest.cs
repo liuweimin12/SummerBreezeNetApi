d0f93ced2798cdfecaf58f0567092948ef4cd58f
zhongluantian <tianzhongluan@163.com>
2016-04-01 14:28:40 +0800

    新建ArchiveUnitTest

---
+++ Yun.UnitTest/ArchiveUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Yun.UnitTest
+{
+    [TestClass]
+    public class ArchiveUnitTest
+    {
+        [TestMethod]
+        public void GetArchivesRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetArchivesRequest
+                {
+                    Ip = "192.168.1.1",
+                    Phone = "12345678996",
+                    Code = "888999",
+                    ShopId = 123,
+                    CompanyId = 123
+                });
+
+            Assert.IsTrue(req.UserId > 0);
+        }
+    }
+}
