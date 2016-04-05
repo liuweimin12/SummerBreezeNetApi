b7070dc21cfe17a0e5177aa11875e88e2c1c4767
zhongluantian <tianzhongluan@163.com>
2016-04-05 14:02:05 +0800

    archive

--- Yun.UnitTest/ArchiveUnitTest.cs
+++ Yun.UnitTest/ArchiveUnitTest.cs
@@ -8,32 +8,32 @@ namespace Yun.UnitTest
     [TestClass]
     public class ArchiveUnitTest
     {
-        [TestMethod]
-        public void GetArchivesRequest()
-        {
-            YunClient.Format = "json";
-            var req =
-                YunClient.Instance.Execute(new GetArchivesRequest
-                {
-                    PageNum = 1,
-                    PageSize = 10,
-                    AuthorId = 123456,
-                    Sort = "123",
-                    Title = "titlt",
-                    StartPostTime=1,
-                    EndPostTime=2,
-                    TaxonomyId=123,
-                    Fields="fields",
-                    ShopId=123,
-                    Status="status",
-                    ///HasPunish=,
-                    Tags="tags",
-                    CategoryName="hello",
-                    CompanyId=123
+        //[TestMethod]
+        //public void GetArchivesRequest()
+        //{
+        //    YunClient.Format = "json";
+        //    var req =
+        //        YunClient.Instance.Execute(new GetArchivesRequest
+        //        {
+        //            PageNum = 1,
+        //            PageSize = 10,
+        //            AuthorId = 123456,
+        //            Sort = "123",
+        //            Title = "titlt",
+        //            StartPostTime=1,
+        //            EndPostTime=2,
+        //            TaxonomyId=123,
+        //            Fields="fields",
+        //            ShopId=123,
+        //            Status="status",
+        //           /// HasPunish='0',
+        //            Tags="tags",
+        //            CategoryName="hello",
+        //            CompanyId=123
                     
-                });
-            Assert.IsTrue(req != null);
-        }
+        //        });
+        //    Assert.IsTrue(req != null);
+        //}
         [TestMethod]
         public void GetArchiveRequest()
         {
@@ -47,32 +47,32 @@ namespace Yun.UnitTest
                 });
             Assert.IsTrue(req != null);
         }
-        [TestMethod]
-        public void GetCategoriesRequest()
-        {
-            YunClient.Format = "json";
-            var req =
-                YunClient.Instance.Execute(new GetCategoriesRequest
-                {
-                    Display = 1,
-                    ShopId= 123,
-                    ParentId=123
+        //[TestMethod]
+        //public void GetCategoriesRequest()
+        //{
+        //    YunClient.Format = "json";
+        //    var req =
+        //        YunClient.Instance.Execute(new GetCategoriesRequest
+        //        {
+        //            Display = 1,
+        //            ShopId= 123,
+        //            ParentId=123
 
-                });
-            Assert.IsTrue(req != null);
-        }
-        [TestMethod]
-        public void GetCategoryRequest()
-        {
-            YunClient.Format = "json";
-            var req =
-                YunClient.Instance.Execute(new GetCategoryRequest
-                {
-                    Id = 123,
+        //        });
+        //    Assert.IsTrue(req != null);
+        //}
+        //[TestMethod]
+        //public void GetCategoryRequest()
+        //{
+        //    YunClient.Format = "json";
+        //    var req =
+        //        YunClient.Instance.Execute(new GetCategoryRequest
+        //        {
+        //            Id = 123,
                     
-                });
-            Assert.IsTrue(req != null);
-        }
+        //        });
+        //    Assert.IsTrue(req != null);
+        //}
        
         }
     }
