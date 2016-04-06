2b0d10381b38afd36c905362f0251d9f08ba155c
zhongluantian <tianzhongluan@163.com>
2016-04-06 16:38:46 +0800

    新建logistics单元测试

---
+++ Yun.UnitTest/LogisticsUnitTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yun.Logistics.Request;
+
+namespace Yun.UnitTest
+{
+    /// <summary>
+    /// MarketingUnitTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class LogisticsUnitTest
+    {
+        [TestMethod]
+        public void GetAddressesRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetAddressesRequest
+                {
+                    Sort = "desc",
+                    AddressType = 0
+                });
+            Assert.IsTrue(req != null);
+        }
+        [TestMethod]
+        public void GetAddressRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetAddressRequest
+                {
+                    Id = 123
+
+                });
+            Assert.IsTrue(req != null);
+        }
+        [TestMethod]
+        public void GetDefaultAddressRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetDefaultAddressRequest
+                {
+                    AddressType = 0
+
+                });
+            Assert.IsTrue(req != null);
+        }
+        [TestMethod]
+        public void GetExpressTemplateRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetExpressTemplateRequest
+                {
+                    Id = 0
+
+                });
+            Assert.IsTrue(req != null);
+
+        }
+        [TestMethod]
+        public void GetTheMostExpensiveTemplateRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetTheMostExpensiveTemplateRequest
+                {
+                    ItemIds = "123"
+                 });
+            Assert.IsTrue(req != null);
+
+        }
+        [TestMethod]
+        public void SaveAddressRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new SaveAddressRequest
+                {
+                    Id = 123,
+                    Province="浙江省",
+                    City="宁波",
+                    Area="高新区",
+                    Street="杨帆街道",
+                    Zipcode="000000",
+                    Name="hello",
+                    Mobile="0000-000000",
+                    IsDefault=0
+
+                });
+            Assert.IsTrue(req != null);
+
+        }
+        [TestMethod]
+        public void SetDefaultAddressRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new SetDefaultAddressRequest
+                {
+                    Id = 123
+                   });
+            Assert.IsTrue(req != null);
+
+        }
+
+    } }
