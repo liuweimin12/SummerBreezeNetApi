d29f6e700cc3e5b03827d6f41bd793905e1d847d
zhongluantian <tianzhongluan@163.com>
2016-04-11 11:09:10 +0800

    更新

--- Yun.UnitTest/ArchiveUnitTest.cs
+++ Yun.UnitTest/ArchiveUnitTest.cs
@@ -8,32 +8,19 @@ namespace Yun.UnitTest
     [TestClass]
     public class ArchiveUnitTest
     {
-        //[TestMethod]
-        //public void GetArchivesRequest()
-        //{
-        //    YunClient.Format = "json";
-        //    var req =
-        //        YunClient.Instance.Execute(new GetArchivesRequest
-        //        {
-        //            PageNum = 1,
-        //            PageSize = 10,
-        //            AuthorId = 123456,
-        //            Sort = "123",
-        //            Title = "titlt",
-        //            StartPostTime=1,
-        //            EndPostTime=2,
-        //            TaxonomyId=123,
-        //            Fields="fields",
-        //            ShopId=123,
-        //            Status="status",
-        //           /// HasPunish='0',
-        //            Tags="tags",
-        //            CategoryName="hello",
-        //            CompanyId=123
-                    
-        //        });
-        //    Assert.IsTrue(req != null);
-        //}
+        [TestMethod]
+        public void GetArchivesRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetArchivesRequest
+                {
+                    PageNum = 1,
+                    PageSize = 10,
+                   });
+            Assert.IsTrue(req != null);
+        }
+
         [TestMethod]
         public void GetArchiveRequest()
         {
@@ -41,64 +28,52 @@ namespace Yun.UnitTest
             var req =
                 YunClient.Instance.Execute(new GetArchiveRequest
                 {
-                    Id = 1,
-                    Fields = "fields"
-                    
                 });
             Assert.IsTrue(req != null);
         }
-        //[TestMethod]
-        //public void GetCategoriesRequest()
-        //{
-        //    YunClient.Format = "json";
-        //    var req =
-        //        YunClient.Instance.Execute(new GetCategoriesRequest
-        //        {
-        //            Display = 1,
-        //            ShopId= 123,
-        //            ParentId=123
 
-        //        });
-        //    Assert.IsTrue(req != null);
-        //}
-        //[TestMethod]
-        //public void GetCategoryRequest()
-        //{
-        //    YunClient.Format = "json";
-        //    var req =
-        //        YunClient.Instance.Execute(new GetCategoryRequest
-        //        {
-        //            Id = 123,
+        [TestMethod]
+        public void GetArchiveCategoriesRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new GetArchiveCategoriesRequest
+                {
+                });
+            Assert.IsTrue(req != null);
+        }
 
-        //        });
-        //    Assert.IsTrue(req != null);
-        //}
+        [TestMethod]
+        public void GetArchiveCategoryRequest()
+        {
+            YunClient.Format = "XML";
+            var req =
+                YunClient.Instance.Execute(new GetArchiveCategoryRequest
+                {
+                });
+            Assert.IsTrue(req != null);
+        }
+
         [TestMethod]
         public void GetArchiveTagsRequest()
         {
-            YunClient.Format = "json";
+            YunClient.Format = "xml";
             var req =
                 YunClient.Instance.Execute(new GetArchiveTagsRequest
                 {
-                    ShopId = 123,
-                    PageNum = 1,
-                    PageSize=20
-
                 });
             Assert.IsTrue(req != null);
         }
+
         [TestMethod]
         public void IncreaseArchiveVisitRequest()
         {
-            YunClient.Format = "json";
+            YunClient.Format = "xml";
             var req =
                 YunClient.Instance.Execute(new IncreaseArchiveVisitRequest
                 {
-                    Id = 123,
-                    Visits = 123,
-                 
-
-                });
+                   
+                 });
             Assert.IsTrue(req != null);
         }
 
