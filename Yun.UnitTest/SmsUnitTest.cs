b79875f58e3656c820a4375dc277790f7744a012
zhongluantian <tianzhongluan@163.com>
2016-04-13 11:18:31 +0800

    新加smsunittest

---
+++ Yun.UnitTest/SmsUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yun.Sms.Request;
+
+
+namespace Yun.UnitTest
+{
+    [TestClass]
+    public class SmsUnitTest
+    {
+        [TestMethod]
+        public void SendShortMessageRequest()
+        {
+            YunClient.Format = "json";
+            var req =
+                YunClient.Instance.Execute(new SendShortMessageRequest
+                {
+                  
+                   });
+            Assert.IsTrue(req != null);
+        }
+        
+      
+
+    }
+    }
+
