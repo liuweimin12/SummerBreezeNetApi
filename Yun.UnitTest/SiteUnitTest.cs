f4e3b2296df793523560f71629792f8607a08020
zhongluantian <tianzhongluan@163.com>
2016-04-14 09:41:14 +0800

    新增SITE UNIT TEST

---
+++ Yun.UnitTest/SiteUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yun.Site.Request;
+
+namespace Yun.UnitTest
+{
+    /// <summary>
+    /// SiteUnitTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class SiteUnitTest
+    {
+        [TestMethod]
+        public void GetImageTextsSiteElementRequest()
+        {
+            YunClient.Format = "json";
+
+            var req =
+                YunClient.Instance.Execute(new GetImageTextsSiteElementRequest
+                {
+                    
+                });
+
+            Assert.IsTrue(req != null);
+        }
+    }
+}
