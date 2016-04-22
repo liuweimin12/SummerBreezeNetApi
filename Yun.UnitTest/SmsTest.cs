b4679f3d42e85087c6f3d1522948b9079f4fc35d
夏日微风 <chengjie.shen@hotmail.com>
2016-04-22 15:21:09 +0800

    提交短信发送代码

---
+++ Yun.UnitTest/SmsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yun.Sms.Request;
+
+namespace Yun.UnitTest
+{
+    [TestClass]
+    public class SmsTest
+    {
+        [TestMethod]
+        public void SendShortMessageRequest()
+        {
+            var req =
+                YunClient.Instance.Execute(new SendShortMessageRequest
+                {
+                    CompanyId = 3,
+                    Content = "您正在进行注册操作，验证码为：123456",
+                    Mobile = "15958805628"
+                });
+
+            Assert.IsTrue(req.Result);
+        }
+    }
+}
