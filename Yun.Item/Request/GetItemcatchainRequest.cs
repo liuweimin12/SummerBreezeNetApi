2ca1b662335e71852eb86b3a8d7188234bf243ea
zhongluantian <tianzhongluan@163.com>
2016-06-13 10:08:40 +0800

    更新测试

--- Yun.Item/Request/GetItemcatchainRequest.cs
+++ Yun.Item/Request/GetItemcatchainRequest.cs
@@ -5,6 +5,9 @@ using Yun.Util;
 
 namespace Yun.Item.Request
 {
+    /// <summary>
+    /// 获取全局类目链
+    /// </summary>
     public class GetItemCatChainRequest : ITopRequest<ItemCatsResponse>
     {
         /// <summary>
