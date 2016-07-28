48945f536818dfa7cd5567ef2c65bb387fd7493b
夏日微风 <chengjie.shen@hotmail.com>
2016-07-28 17:01:21 +0800

    增加requestid

--- Yun/Response/YunResponse.cs
+++ Yun/Response/YunResponse.cs
@@ -8,39 +8,35 @@ namespace Yun.Response
     public abstract class YunResponse
     {
         /// <summary>
+        /// 请求ID
+        /// </summary>
+        [XmlElement("request_id")]
+        public string RequestId { get; set; }
+
+        /// <summary>
         /// 错误码
         /// </summary>
         [XmlElement("code")]
-
         public string ErrCode { get; set; }
 
         /// <summary>
         /// 错误信息
         /// </summary>
         [XmlElement("msg")]
-
         public string ErrMsg { get; set; }
 
         /// <summary>
         /// 子错误码
         /// </summary>
         [XmlElement("sub_code")]
-
         public string SubErrCode { get; set; }
 
         /// <summary>
         /// 子错误信息
         /// </summary>
         [XmlElement("sub_msg")]
-
         public string SubErrMsg { get; set; }
 		
-		/// <summary>
-        /// 禁止访问字段
-        /// </summary>
-        //[XmlElement("top_forbidden_fields")]
-        //public string TopForbiddenFields { get; set; }
-
         /// <summary>
         /// 响应原始内容
         /// </summary>
