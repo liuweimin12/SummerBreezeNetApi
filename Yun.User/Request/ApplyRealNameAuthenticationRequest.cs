35c63b0d03dc2979da8255fb9b57d274c5270ebd
夏日微风 <chengjie.shen@hotmail.com>
2016-08-04 08:53:54 +0800

    增加实名认证部分

---
+++ Yun.User/Request/ApplyRealNameAuthenticationRequest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Yun.Interface;
+using Yun.Response;
+
+namespace Yun.User.Request
+{
+    /// <summary>
+    /// 申请实名认证请求
+    /// yun.user.realnameauthentication.apply
+    /// </summary>
+    public class ApplyRealNameAuthenticationRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 认证名字
+        /// </summary>
+        public string AuthenticationName { get; set; }
+
+        /// <summary>
+        /// 身份证号码
+        /// </summary>
+        public string IdCard { get; set; }
+
+        /// <summary>
+        /// 身份证正面
+        /// </summary>
+        public string PositiveIdentityCard { get; set; }
+
+        /// <summary>
+        /// 身份证背面
+        /// </summary>
+        public string BackOfIdCard { get; set; }
+
+
+        /// <summary>
+        /// 身份证手持
+        /// </summary>
+        public string IdCardHandheld { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.user.realnameauthentication.apply";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"authenticationname",AuthenticationName},
+                {"idcard",IdCard},
+                {"positiveIdentitycard",PositiveIdentityCard},
+                {"backofIdcard",BackOfIdCard},
+                {"idcardhandheid",IdCardHandheld}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+        }
+    }
+}
---
+++ Yun.User/Request/RefuseRealNameAuthenticationRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.User.Request
+{
+    /// <summary>
+    /// 拒绝实名认证申请
+    /// yun.user.realnameauthentication.refuse
+    /// </summary>
+    public class RefuseRealNameAuthenticationRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 拒绝理由
+        /// </summary>
+        public string Reason { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.user.realnameauthentication.refuse";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"userid",UserId},
+                {"reason",Reason}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("userid", UserId);
+        }
+    }
+}
