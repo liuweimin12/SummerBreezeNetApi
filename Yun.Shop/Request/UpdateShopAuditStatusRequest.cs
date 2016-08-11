6f00ac20bfe0c3b4b89d5ea000cdb024cf2375b1
夏日微风 <chengjie.shen@hotmail.com>
2016-08-11 17:47:09 +0800

    新增店铺审核的方法

---
+++ Yun.Shop/Request/UpdateShopAuditStatusRequest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+
+namespace Yun.Shop.Request
+{
+    /// <summary>
+    /// 更新店铺审核状态
+    /// yun.shop.auditstatus.update
+    /// </summary>
+    public class UpdateShopAuditStatusRequest:ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 店铺ID
+        /// </summary>
+        public int ShopId { get; set; }
+
+        /// <summary>
+        /// 认证状态，0：未认证；1：已申请；2：已通过；-1:拒绝
+        /// </summary>
+        public int Status { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.shop.auditstatus.update";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"shopid", ShopId},
+                {"status", Status},
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+
+        }
+    }
+}
