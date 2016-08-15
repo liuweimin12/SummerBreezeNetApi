7bbec39e7616a9cde8db70fbb6ae6c5211e151e8
夏日微风 <chengjie.shen@hotmail.com>
2016-08-15 15:02:08 +0800

    购物车的更新方法增加SKUID的变更

--- Yun.Trade/Request/UpdateCartRequest.cs
+++ Yun.Trade/Request/UpdateCartRequest.cs
@@ -7,11 +7,25 @@ namespace Yun.Trade.Request
 {
     public class UpdateCartRequest : ITopRequest<BoolResultResponse>
     {
+        /// <summary>
+        /// 购物车ID
+        /// </summary>
         public int CartId { get; set; }
 
+        /// <summary>
+        /// 数量
+        /// </summary>
         public int Quantity { get; set; }
 
+        /// <summary>
+        /// 用户标识
+        /// </summary>
         public string UserFlag { get; set; }
+
+        /// <summary>
+        /// 所选择的SKUID，如果为空，则不变更
+        /// </summary>
+        public int? SkuId { get; set; }
 
         public string GetApiName()
         {
@@ -24,7 +38,8 @@ namespace Yun.Trade.Request
             {
                 {"cartid", CartId},
                 {"quantity", Quantity},
-                {"userflag",UserFlag}
+                {"userflag",UserFlag},
+                {"skuid",SkuId }
             };
 
             return parameters;
