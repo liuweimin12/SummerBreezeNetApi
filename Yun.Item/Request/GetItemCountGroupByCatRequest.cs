226077d56b727c0d864f0bcf020229b81cea0c9a
夏日微风 <chengjie.shen@hotmail.com>
2016-08-16 09:12:46 +0800

    增加 根据商品关键子获取商品在类目下的商品数量 的方法

---
+++ Yun.Item/Request/GetItemCountGroupByCatRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Item.Response;
+using Yun.Util;
+
+namespace Yun.Item.Request
+{
+    /// <summary>
+    /// 根据商品关键子获取商品在类目下的商品数量
+    /// yun.item.count.groupbycat.get
+    /// </summary>
+    public class GetItemCountGroupByCatRequest :ITopRequest<GetItemCountGroupByCatResponse>
+    {
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        public string GoodsName { get; set; }
+
+
+        public string GetApiName()
+        {
+            return "yun.item.count.groupbycat.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"goodsname", GoodsName}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("goodsname", GoodsName);
+        }
+    }
+}
---
+++ Yun.Item/Response/GetItemCountGroupByCatResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using Yun.Response;
+
+namespace Yun.Item.Response
+{
+    [XmlRoot("get_itemcount_groupby_cat_response")]
+    public class GetItemCountGroupByCatResponse : YunResponse
+    {
+        [XmlArray("items_group_by_cat")]
+        [XmlArrayItem("item_group_by_cat")]
+        public List<ItemGroupByCat> ItemsGroupByCat { get; set; }
+    }
+}
