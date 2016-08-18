9d6d624f9120cd290bd8be56b5ca008135531b09
zhongluantian <tianzhongluan@163.com>
2016-08-18 09:51:57 +0800

    新增

---
+++ Yun.Item/ItemGroupByCat.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Serialization;
+
+namespace Yun.Item
+{
+    public class ItemGroupByCat
+    {
+        /// <summary>
+        /// 关键字
+        /// </summary>
+        [XmlElement("keywords")]
+        public string KeyWords { get; set; }
+
+        /// <summary>
+        /// 分类ID，最后一级
+        /// </summary>
+        [XmlElement("cat_id")]
+        public int CatId { get; set; }
+
+        /// <summary>
+        /// 分类名称
+        /// </summary>
+        [XmlElement("cat_name")]
+        public string CatName { get; set; }
+
+
+        /// <summary>
+        /// 商品数量
+        /// </summary>
+        [XmlElement("goods_count")]
+        public int GoodsCount { get; set; }
+
+
+    }
+}
