e75a967b5c58352bf63a7d3361215ac59464bf7d
zhongluantian <tianzhongluan@163.com>
2016-08-18 09:55:30 +0800

    新增

---
+++ Yun.Shop/UserAssociation.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Xml.Serialization;
+
+namespace Yun.Shop
+{
+    public class UserAssociation
+    {
+        /// <summary>
+        /// 收藏店铺的时间
+        /// </summary>
+        [XmlElement("collected_time")]
+        public string CollectedTime { get; set; }
+    }
+}
