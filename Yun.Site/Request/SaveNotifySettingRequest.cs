47ee89595f38b1f4a77862e7507206e8caf1742b
zhongluantian <tianzhongluan@163.com>
2016-08-18 09:53:13 +0800

    新增

---
+++ Yun.Site/Request/SaveNotifySettingRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+
+namespace Yun.Site.Request
+{
+    /// <summary>
+    /// 设置系统通知信息
+    /// yun.setting.notification.save
+    /// </summary>
+    public class SaveNotifySettingRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 通知设置
+        /// </summary>
+        public List<NotifySettingJson> NotifySetting { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.setting.notification.save";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"notifysetting",NotifySetting}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+
+        }
+    }
+}
