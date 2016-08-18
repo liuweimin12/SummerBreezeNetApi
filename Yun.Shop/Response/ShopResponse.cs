505d5d74ba20ebefbfca56662dfba4f7c70dcec6
zhongluantian <tianzhongluan@163.com>
2016-08-18 09:54:51 +0800

    更新

--- Yun.Shop/Response/ShopResponse.cs
+++ Yun.Shop/Response/ShopResponse.cs
@@ -3,6 +3,7 @@ using Yun.Response;
 
 namespace Yun.Shop.Response
 {
+    [XmlRoot("get_shop_response")]
     public class ShopResponse : YunResponse
     {
         /// <summary>
@@ -10,5 +11,12 @@ namespace Yun.Shop.Response
         /// </summary>
         [XmlElement("shop")]
         public ShopDetail Shop { get; set; }
+
+
+        /// <summary>
+        /// 用户关联属性，在用户登录后可见
+        /// </summary>
+        [XmlElement("user_association")]
+        public UserAssociation UserAssociation { get; set; }
     }
 }
