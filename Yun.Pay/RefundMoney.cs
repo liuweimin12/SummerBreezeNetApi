90c6a49b7dfc0072d065aa232eec8649451d5092
summer-breeze <chengjie.shen@hotmail.com>
2016-09-11 11:22:24 +0800

    增加获取最大克退款金额的方法

---
+++ Yun.Pay/RefundMoney.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Xml.Serialization;
+
+namespace Yun.Pay
+{
+    public class RefundMoney
+    {
+        /// <summary>
+        /// 余额
+        /// </summary>
+        [XmlElement("balance")]
+        public double Balance { get; set; }
+
+        /// <summary>
+        /// 在线金额
+        /// </summary>
+        [XmlElement("online_money")]
+        public double OnlineMoney { get; set; }
+
+        /// <summary>
+        /// 充值卡金额
+        /// </summary>
+        [XmlElement("prepaidcard_money")]
+        public double PrepaidCardMoney { get; set; }
+
+        /// <summary>
+        /// 现金/线下金额
+        /// </summary>
+        [XmlElement("cash")]
+        public double Cash { get; set; }
+    }
+}
