221349ffb92202e9c7abb1d5a36fa96dde0bed99
summer-breeze <chengjie.shen@hotmail.com>
2016-10-01 23:37:09 +0800

    增加清除成数据的方法

---
+++ Yun.Site/Request/ClearCityRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+
+namespace Yun.Site.Request
+{
+    /// <summary>
+    /// 清除自定义的城市数据
+    /// yun.cities.clear
+    /// </summary>
+    public class ClearCityRequest : ITopRequest<BoolResultResponse>
+    {
+        public string GetApiName()
+        {
+            return "yun.cities.clear";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+
+        }
+    }
+}
