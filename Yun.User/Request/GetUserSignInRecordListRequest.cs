cab9ef5b93617c5b6be768b67a076af9ae31f74d
夏日微风 <chengjie.shen@hotmail.com>
2016-10-25 17:09:23 +0800

    增加用户签到的相关方法

---
+++ Yun.User/Request/GetUserSignInRecordListRequest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.User.Response;
+using Yun.Util;
+
+namespace Yun.User.Request
+{
+    /// <summary>
+    /// 获取用户的签到历史记录
+    /// yun.user.signin.record.list.get
+    /// </summary>
+    public class GetUserSignInRecordListRequest : ITopRequest<GetUserSignInRecordListResponse>
+    {
+        /// <summary>
+        /// 页码
+        /// </summary>
+        public int PageNum
+        {
+            get { return _pageNum; }
+            set
+            {
+                _pageNum = value < 1 ? 1 : value;
+            }
+        }
+
+        private int _pageNum = 1;
+
+        /// <summary>
+        /// 页容量
+        /// </summary>
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set
+            {
+                _pageSize = value < 1 ? 1 : value;
+            }
+        }
+
+        private int _pageSize = 1;
+
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int UserId { get; set; }
+
+
+        public string GetApiName()
+        {
+            return "yun.user.signin.record.list.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"pagenum", PageNum},
+                {"pagesize", PageSize},
+                {"userid", UserId},
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("userid", UserId);
+            RequestValidator.ValidateMinValue("userid", UserId, 1);
+        }
+    }
+}
---
+++ Yun.User/Request/GetUserSignInStatisticsRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.User.Response;
+
+namespace Yun.User.Request
+{
+    /// <summary>
+    /// 获取用户的签到统计数据
+    /// yun.user.signin.statistics.get
+    /// </summary>
+    public class GetUserSignInStatisticsRequest : ITopRequest<GetUserSignInStatisticsResponse>
+    {
+        public string GetApiName()
+        {
+            return "yun.user.signin.statistics.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            return new YunDictionary();
+        }
+
+        public void Validate()
+        {
+
+        }
+    }
+}
