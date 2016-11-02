f0411d4822327305230cd847fbea21c89a7553fc
夏日微风 <chengjie.shen@hotmail.com>
2016-11-02 10:48:17 +0800

    修改变更用户积分的方法

--- Yun.User/Request/ModifyUserCreditRequest.cs
+++ Yun.User/Request/ModifyUserCreditRequest.cs
@@ -23,7 +23,13 @@ namespace Yun.User.Request
 
 
         /// <summary>
-        /// 变更原因
+        /// 用户ID
+        /// </summary>
+        public int TargetUserId { get; set; }
+
+
+        /// <summary>
+        /// 备注
         /// </summary>
         public string Remark { get; set; }
 
@@ -31,6 +37,16 @@ namespace Yun.User.Request
         /// 过期天数
         /// </summary>
         public int ExpiredDay { get; set; }
+
+        /// <summary>
+        /// 变更详情，不能超过20个字
+        /// </summary>
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// 对应的关系ID，只允许传入英文，数字
+        /// </summary>
+        public string DetailId { get; set; }
 
 
         public string GetApiName()
@@ -42,19 +58,21 @@ namespace Yun.User.Request
         {
             var parameters = new YunDictionary
             {
-                {"usernick",UserNick},
-                {"credit",Credit},
-                {"expiredday",ExpiredDay},
-                {"remark",Remark}
+                {"usernick", UserNick},
+                {"credit", Credit},
+                {"expiredday", ExpiredDay},
+                {"remark", Remark},
+                {"detail", Detail},
+                {"detailid", DetailId},
+                {"targetuserid",TargetUserId }
             };
             return parameters;
         }
 
         public void Validate()
         {
-            RequestValidator.ValidateRequired("remark", Remark);
+            RequestValidator.ValidateRequired("detail", Detail);
             RequestValidator.ValidateRequired("credit", Credit);
-            RequestValidator.ValidateRequired("usernick", UserNick);
         }
     }
 }
