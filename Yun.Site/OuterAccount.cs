1546e20d365182ff13105fbbe0f62f483406540c
夏日微风 <chengjie.shen@hotmail.com>
2016-11-17 17:34:19 +0800

    增加创建设备的初始化信息接口

---
+++ Yun.Site/OuterAccount.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Yun.Site
+{
+    public class OuterAccount
+    {
+        public string name { get; set; }
+
+        public string token { get; set; }
+    }
+}
---
+++ Yun.Site/Request/CreateDeviceInitializationRequest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Site.Request
+{
+    /// <summary>
+    /// 创建设备的初始化信息
+    /// yun.device.initialization.info.create
+    /// </summary>
+    public class CreateDeviceInitializationRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 设备ID
+        /// </summary>
+        public string DeviceId { get; set; }
+
+        /// <summary>
+        /// 外部的Token数据
+        /// </summary>
+        public IList<OuterAccount> OuterData { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.device.initialization.info.create";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"deviceid", DeviceId},
+                {"outerdata", OuterData},
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("deviceid", DeviceId);
+        }
+    }
+}
