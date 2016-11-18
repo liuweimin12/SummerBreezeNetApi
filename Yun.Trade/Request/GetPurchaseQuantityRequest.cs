aafb7ca483ca7e3dd3fd3836a6a5548aaf8162be
summer-breeze <chengjie.shen@hotmail.com>
2016-11-18 22:25:15 +0800

    增加获取已购买数量的方法

---
+++ Yun.Trade/Request/GetPurchaseQuantityRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Trade.Request
+{
+
+    /// <summary>
+    /// 获取商品的购买数量
+    /// yun.item.purchase.quantity.get
+    /// </summary>
+    public class GetPurchaseQuantityRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int ItemId { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.item.purchase.quantity.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"itemid", ItemId}
+            };
+
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("itemid", ItemId);
+        }
+    }
+}
