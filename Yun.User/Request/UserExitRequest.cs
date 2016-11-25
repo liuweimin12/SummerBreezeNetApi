7926715dde87ed68505277d82a64e5d2d371059b
夏日微风 <chengjie.shen@hotmail.com>
2016-11-25 16:35:43 +0800

    增加用户退出接口

---
+++ Yun.User/Request/UserExitRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+
+namespace Yun.User.Request
+{
+    /// <summary>
+    /// 用户退出
+    /// yun.user.exit
+    /// </summary>
+    public class UserExitRequest : ITopRequest<IntResultResponse>
+    {
+        public string GetApiName()
+        {
+            return "yun.user.exit";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            return new Dictionary<string, string>();
+        }
+
+        public void Validate()
+        {
+        }
+    }
+}
