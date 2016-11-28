9ca051b49868664156f15e1d27530148e2d659b1
夏日微风 <chengjie.shen@hotmail.com>
2016-11-28 15:01:49 +0800

    增加增加商品流量接口

---
+++ Yun.Item/Request/IncreaseItemViewsRequest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Item.Request
+{
+    /// <summary>
+    /// 更新商品浏览量
+    /// yun.item.views.increase
+    /// </summary>
+    public class IncreaseItemViewsRequest: ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int ItemId { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.item.views.increase";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"itemid",ItemId}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("itemid", ItemId);
+        }
+    }
+}
