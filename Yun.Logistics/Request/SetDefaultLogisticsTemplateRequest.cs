fc15a54fcc97a16adb2263c8333f3751e866ebce
summer-breeze <chengjie.shen@hotmail.com>
2016-12-04 23:38:48 +0800

    增加设为默认物流模板的接口

---
+++ Yun.Logistics/Request/SetDefaultLogisticsTemplateRequest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Logistics.Request
+{
+    /// <summary>
+    /// 设为默认物流模板
+    /// yun.logistics.template.default.set
+    /// </summary>
+    public class SetDefaultLogisticsTemplateRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 物流模板ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 是否默认
+        /// </summary>
+        public bool? IsDefault { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.logistics.template.default.set";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"id",Id},
+                {"isdefault",IsDefault }
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateMinValue("id", Id, 1);
+            RequestValidator.ValidateRequired("isdefault", IsDefault);
+        }
+    }
+}
