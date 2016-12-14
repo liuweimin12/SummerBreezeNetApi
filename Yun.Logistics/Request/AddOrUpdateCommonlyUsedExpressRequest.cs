7cdddeabab003860a1f969ccf4199024051f43c2
summer-breeze <chengjie.shen@hotmail.com>
2016-12-14 22:47:04 +0800

    增加常用快递的功能

---
+++ Yun.Logistics/Request/AddOrUpdateCommonlyUsedExpressRequest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Logistics.Request
+{
+    /// <summary>
+    /// 保存常用快递列表
+    /// yun.logistics.commonlyused.express.addorupdate
+    /// </summary>
+    public class AddOrUpdateCommonlyUsedExpressRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 常用快递
+        /// </summary>
+        public string ExpressName { get; set; }
+
+        /// <summary>
+        /// 是否默认
+        /// </summary>
+        public bool IsDefault { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.logistics.commonlyused.express.addorupdate";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"expressname", ExpressName},
+                {"isdefault", IsDefault}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("expressname", ExpressName);
+        }
+    }
+}
---
+++ Yun.Logistics/Request/DeleteCommonlyUsedExpressRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Logistics.Request
+{
+    /// <summary>
+    /// 删除常用的快递公司
+    /// yun.logistics.commonlyused.express.delete
+    /// </summary>
+    public class DeleteCommonlyUsedExpressRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.logistics.commonlyused.express.delete";
+        }
+
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"id", Id}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateMinValue("id", Id, 1);
+        }
+    }
+}
