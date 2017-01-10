8babd2145dd066178ad80587968655fda832f9cf
夏日微风 <chengjie.shen@hotmail.com>
2017-01-10 16:09:25 +0800

    增加获取积分支出来源的方法

---
+++ Yun.User/SourcesOfExpenditure.cs
@@ -0,0 +1,64 @@
+﻿using System.Xml.Serialization;
+
+namespace Yun.User
+{
+    public class SourcesOfExpenditure
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        [XmlElement("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        [XmlElement("expenditure_user_id")]
+        public int ExpenditureUserId { get; set; }
+
+
+        /// <summary>
+        /// 收入人ID
+        /// </summary>
+        [XmlElement("income_user_id")]
+        public int IncomeUserId { get; set; }
+
+
+        /// <summary>
+        /// 支出ID
+        /// </summary>
+        [XmlElement("expenditure_id")]
+        public int ExpenditureId { get; set; }
+
+        /// <summary>
+        /// 收入ID
+        /// </summary>
+        [XmlElement("income_id")]
+        public int IncomeId { get; set; }
+
+
+        /// <summary>
+        /// 积分
+        /// </summary>
+        [XmlElement("score")]
+        public int Score { get; set; }
+
+        /// <summary>
+        /// 来源类型
+        /// </summary>
+        [XmlElement("income_type")]
+        public string IncomeType { get; set; }
+
+        /// <summary>
+        /// 支出类型
+        /// </summary>
+        [XmlElement("expenditure_type")]
+        public string ExpenditureType { get; set; }
+
+        /// <summary>
+        /// 支出时间
+        /// </summary>
+        [XmlElement("expenditure_time")]
+        public string ExpenditureTime { get; set; }
+    }
+}
