ad1e7f06bda06bb258e500b0a83c5c43d5dcaafd
夏日微风 <chengjie.shen@hotmail.com>
2017-01-20 13:30:11 +0800

    增加商品匹配物流模板的方法

---
+++ Yun.Item/Request/MatchingLogisticsTemplatesRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Item.Request
+{
+
+    /// <summary>
+    /// 匹配商品的物流模板
+    /// yun.item.logistics.templates.matching
+    /// </summary>
+    public class MatchingLogisticsTemplatesRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public string ItemIds { get; set; }
+
+        /// <summary>
+        /// 物流模板ID
+        /// </summary>
+        public int LogisticsTemplateId { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.item.logistics.templates.matching";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"logisticstemplateid",LogisticsTemplateId},
+                {"itemids",ItemIds}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("itemids", ItemIds);
+            RequestValidator.ValidateMinValue("logisticstemplateid", LogisticsTemplateId, 1);
+        }
+    }
+}
