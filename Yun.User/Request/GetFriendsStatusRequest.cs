c7841c9574b0e4c4f43a819698f8016b9c4424a3
夏日微风 <chengjie.shen@hotmail.com>
2017-03-08 16:38:45 +0800

    获取批量查询好友状态的接口

---
+++ Yun.User/Request/GetFriendsStatusRequest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.User.Response;
+using Yun.Util;
+
+namespace Yun.User.Request
+{
+    /// <summary>
+    /// 获取邀请状态
+    /// yun.user.friends.status.get
+    /// </summary>
+    public class GetFriendsStatusRequest : ITopRequest<GetFriendsStatusResponse>
+    {
+        /// <summary>
+        /// 对方用户ID，多个用英文逗号分隔
+        /// </summary>
+        public string OtherUserIds { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.user.friends.status.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+               {"otheruserids",OtherUserIds}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("otheruserids", OtherUserIds);    
+        }
+    }
+}
---
+++ Yun.User/Response/GetFriendsStatusResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using Yun.Response;
+
+namespace Yun.User.Response
+{
+    /// <summary>
+    /// 批量查询好友的状态
+    /// </summary>
+    [XmlRoot("get_friends_status_response")]
+    public class GetFriendsStatusResponse : YunResponse
+    {
+        [XmlArray("friend_statuses")]
+        [XmlArrayItem("friend_status")]
+        public List<InviteStatus> FriendStatuses { get; set; }
+    }
+}
