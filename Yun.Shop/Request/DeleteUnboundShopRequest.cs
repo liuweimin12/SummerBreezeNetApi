f3a99745472a9dfb6433a66ff8619bc03935bb94
夏日微风 <chengjie.shen@hotmail.com>
2017-03-09 15:22:37 +0800

    增加删除未绑定店铺的接口

---
+++ Yun.Shop/Request/DeleteUnboundShopRequest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Shop.Request
+{
+    /// <summary>
+    /// 删除未绑定的店铺
+    /// yun.shop.unbound.delete
+    /// </summary>
+    public class DeleteUnboundShopRequest:ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 店铺ID
+        /// </summary>
+        public int Id { get; set; }
+
+        public string GetApiName()
+        {
+            return "yun.shop.unbound.delete";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+               {"id",Id}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("id", Id);
+        }
+    }
+}
