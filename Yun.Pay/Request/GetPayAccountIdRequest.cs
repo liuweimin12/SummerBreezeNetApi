b6e9fc9bd6af2c8f1b126915ddb866b7fc64668e
summer-breeze <chengjie.shen@hotmail.com>
2017-03-12 01:08:52 +0800

    增加获取支付账号的ID的方法

---
+++ Yun.Pay/Request/GetPayAccountIdRequest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Yun.Interface;
+using Yun.Response;
+using Yun.Util;
+
+namespace Yun.Pay.Request
+{
+    /// <summary>
+    /// 获取支付账号的ID
+    /// yun.pay.payaccount.id.get
+    /// </summary>
+    public class GetPayAccountIdRequest : ITopRequest<IntResultResponse>
+    {
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 类型，SHOP,COMPANY,USER
+        /// </summary>
+        public string ContentType { get; set; }
+
+
+        public string GetApiName()
+        {
+            return "yun.pay.payaccount.id.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"content", Content},
+                {"contenttype", ContentType}
+            };
+            return parameters;
+        }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("content", Content);
+            RequestValidator.ValidateRequired("contenttype", ContentType);
+        }
+    }
+}
