0219d6dfb141fc055288b71f700cd1826cf59b5e
aimar_luo <aimar_luo@163.com>
2017-03-22 23:53:02 +0800

    新增获取版本号详情接口-yun.site.version.get

---
+++ Yun.Site/Request/GetSiteVersionRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Yun.Interface;
+using Yun.Site.Response;
+using Yun.Util;
+
+namespace Yun.Site.Request
+{
+    /// <summary>
+    /// 获取版本号详情
+    /// yun.site.version.get
+    /// </summary>
+    public class GetSiteVersionRequest : ITopRequest<GetSiteVersionResponse>
+    {
+
+        /// <summary>
+        /// 版本号ID
+        /// </summary>
+        public int VersionId { get; set; }
+
+        public void Validate()
+        {
+            RequestValidator.ValidateRequired("versionid", VersionId);
+            RequestValidator.ValidateMinValue("versionid", VersionId, 1);
+        }
+
+        public string GetApiName()
+        {
+            return "yun.site.version.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new YunDictionary
+            {
+                {"versionid",VersionId}
+            };
+            return parameters;
+        }
+
+    }
+}
---
+++ Yun.Site/Response/GetSiteVersionResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using Yun.Response;
+
+namespace Yun.Site.Response
+{
+    [XmlRoot("get_site_version_response")]
+    public class GetSiteVersionResponse : YunResponse
+    {
+        [XmlElement("site_version")]
+        public SiteVersion SiteVersion { get; set; }
+    }
+}
